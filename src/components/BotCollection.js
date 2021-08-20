6b43dbd84afc101c42e893a3fd373f53e9558911
Kalex <kalexadamson@hotmail.com>
2021-08-19 20:24:40 -0500

    commit tree 6e316ce7668cfc51c8a27e260410ac422788865e

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,11 +1,11 @@
 import React from "react";
-
-function BotCollection() {
+import BotCard from './BotCard'
+function BotCollection({ bots, clickHandler}) {
   // Your code here
   return (
     <div className="ui four column grid">
       <div className="row">
-        {/*...and here..*/}
+        {bots.map(bot => <BotCard id={bot.id} bot={bot} clickHandler={clickHandler} />)}
         Collection of all bots
       </div>
     </div>
--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,14 +1,37 @@
 import React from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
+import {useState, useEffect} from 'react'
 
 function BotsPage() {
   //start here with your code for step one
 
+  
+
+const API = 'http://localhost:8002/bots'
+const [bots, setBots] = useState([])
+
+useEffect(() => {
+  fetch(API)
+  .then(res => res.json())
+  .then(resData => setBots(resData))
+  
+}, [])
+
+const [botID, SetBotID] = useState("")
+const [myBotArmy, setMyBotArmy] = useState([])
+let armyBot = bots.filter(armyBot => armyBot.id === botID)
+
+let clickHandler = (id) => {
+ SetBotID(id)
+ setMyBotArmy({...myBotArmy, botID})
+}
+
+console.log(myBotArmy)
   return (
     <div>
-      <YourBotArmy />
-      <BotCollection />
+      <YourBotArmy myBotArmy={myBotArmy}/>
+      <BotCollection bots={bots} clickHandler={clickHandler}/>
     </div>
   )
 }
